2e0ce3639ef91fc7e2e00370a7a6cda40b4704a3
Aunt D <dzavizistur@gmail.com>
2025-04-11 12:59:51 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,14 @@
+namespace N10_YP__Task_1_10._04._2025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("helo");
+        }
+    }
+}
